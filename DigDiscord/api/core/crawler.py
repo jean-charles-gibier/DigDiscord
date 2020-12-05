7b4f8a30c792d945b1165cbc41b28b0f89edbade
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-12-05 22:42:46 +0100

    commande getsrvmsg fonctionnelle

--- DigDiscord/api/core/crawler.py
+++ DigDiscord/api/core/crawler.py
@@ -17,11 +17,15 @@ class Crawler:
         """ defines config properties"""
         self.d_token = d_token
         self.d_end_point = d_end_point
+        self.msg_list = []
+        self.channel = ""
+
 
     def _special_get(self, url: str, payload: str, headers: dict, tries: int = 3):
         """
         special_get fetch with special strategy
         to handle http responses & possible errors
+        recursive function.
         url : url to request
         payload : json params
         tries : nb to try
@@ -40,54 +44,64 @@ class Crawler:
 
             if do_retry or response.status_code >= 500:
                 sleep(300)
-                return self.special_get(url, payload, tries - 1)
+                return self._special_get(url, payload, headers, tries - 1)
 
         return response
 
-    def fetch(self, channel: str, nb_messags: int = 0, chunk_size: int = 50) -> str:
-        w = codecs.getwriter("utf-8")(sys.stdout.buffer)
-
-        _last_msg_number = 0
-        _msg_counter = 0
+    def fetch(self, channel: str, nb_messages: int = 0):
+        """
+        get specific nb msg from channel
+        :param channel: id channel
+        :param nb_messages: limit of nb msg
+        :return:
+        """
+        msg_counter = 0
         payload = {}
         headers = {'Authorization': self.d_token}
-        print('CHECK : {} TOKEN : {} ... '.format(channel, self.d_token))
-
-        big_bag_msg = []
+        self.channel = channel
+        self.msg_list = []
 
         while True:
             try:
-                response = self._special_get(
-                    self.d_end_point.format(channel),
-                    payload,
-                    headers)
+                url_end_point = self.d_end_point.format(channel)
+                response = self._special_get(url_end_point, payload, headers)
                 data = response.json()
-                _last_msg_number = _last_msg_number + 1
-                data.reverse()
+                nb_read = len(data)
 
-                big_bag_msg = [{
+                if nb_read == 0:
+                    break
+
+                data.reverse()
+                self.msg_list = [{
                     'date': msg['timestamp'],
                     'author': msg['author']['username'],
                     'content': msg['content'],
                     'mentions': msg['mentions']
-                } for msg in data] + big_bag_msg
+                } for msg in data] + self.msg_list
 
                 payload['before'] = data[0]['id']
-                _msg_counter = _msg_counter + len(data)
+                msg_counter = msg_counter + nb_read
 
-                if nb_messags < _msg_counter:
-                    big_bag_msg = big_bag_msg[:_msg_counter]
+                if nb_messages < msg_counter:
+                    self.msg_list = self.msg_list[:nb_messages]
                     break
-                if len(data) < chunk_size:
+                elif 0 < nb_messages <= msg_counter:
                     break
-                else:
-                    print('Getting {} msg before {} ... '.format(chunk_size, payload['before']))
 
             except AttributeError as err:
                 print("Unexpected error: {0}".format(err))
                 break
+
             except Exception:
                 print("Unexpected error:", sys.exc_info()[0])
-# write a part
-        with open("fetch_{}.json".format(channel), 'w') as myfile:
-            myfile.write(json.dumps(big_bag_msg))
+                break
+
+
+    def persist(self):
+        """
+        write crawled content
+        :return:
+        """
+        w = codecs.getwriter("utf-8")(sys.stdout.buffer)
+        with open("fetch_{}.json".format(self.channel), 'w') as myfile:
+            myfile.write(json.dumps(self.msg_list))
--- DigDiscord/api/management/commands/getsrvmsg.py
+++ DigDiscord/api/management/commands/getsrvmsg.py
@@ -1,29 +1,30 @@
 """
 getsrvmsg
 Commande de chargement des messages d'un ou plusieurs forum(s) d'un serveur
-identifié(s) par son/ses identifiant(s) de channel
-
+identifié(s) par son/ses identifiant(s) de channel.
 """
 from django.core.management.base import BaseCommand, CommandError
-from api.core import base_utils as bu, crawler as cr
+from api.core.base_utils import Configuration
+from api.core.crawler  import Crawler
 
 
 class Command(BaseCommand):
     help = 'Loads specified server messages'
 
     def add_arguments(self, parser):
-        parser.add_argument('id_channels', nargs='+', type=int)
+        parser.add_argument('--id_channels', nargs='*', type=int)
+        parser.add_argument('--limit', nargs='?', type=int)
 
     def handle(self, *args, **options):
-        discord_token = bu.Configuration.get_from_env('DISCORD_USER_TOKEN', 'NONE')
-        base_end_point = bu.Configuration.get_from_env('BASE_END_POINT', 'NONE')
-        self.stdout.write(self.style.SUCCESS('Get discord token : "%s"' % discord_token))
-        self.stdout.write(self.style.SUCCESS('Get discord end_point : "%s"' % base_end_point))
-        crawler = cr.Crawler(discord_token, base_end_point)
+        discord_token = Configuration.findenv('DISCORD_USER_TOKEN', 'NONE')
+        base_end_point = Configuration.findenv('BASE_END_POINT', 'NONE')
+        crawler = Crawler(discord_token, base_end_point)
 
+        limit = options['limit'] or 100
         for channel_id in options['id_channels']:
             try:
-                crawler.fetch(channel_id, 100)
+                crawler.fetch(channel_id, limit)
+                crawler.persist()
             except Exception:
                 raise CommandError('Channel id "%s" does not exist' % channel_id)
 
