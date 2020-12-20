d8c37eabec3c1bc813da73d3c0c5be4f26152a78
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-12-20 22:25:17 +0100

    Update deploy_ci.py

--- DigDiscord/DigDiscord/settings/deploy_ci.py
+++ DigDiscord/DigDiscord/settings/deploy_ci.py
@@ -16,7 +16,6 @@ DATABASES = {
         "TEST": {
             "USER": "root",
             "NAME": "digdiscord_test",
-#           "PASSWORD": "MOTDEPASSE1234",
             "CHARSET": "utf8mb4",
             "COLLATION": "utf8mb4_unicode_ci",
         },
