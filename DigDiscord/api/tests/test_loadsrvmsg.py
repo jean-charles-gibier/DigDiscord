67f197ce5a376400021544c72c15379f063c2f62
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-12-20 23:58:28 +0100

    Update test_loadsrvmsg.py

--- DigDiscord/api/tests/test_loadsrvmsg.py
+++ DigDiscord/api/tests/test_loadsrvmsg.py
@@ -3,12 +3,15 @@ Test des commandes d'administration custom
 """
 from django.core.management import call_command
 from django.test import TransactionTestCase
+import os
 
 
 class LoadSrvMsg(TransactionTestCase):
     def setUp(self):
         """ fixtures """
-
+        
+    @unittest.skipIf(
+            os.getenv(name,'DJANGO_SETTINGS_MODULE') == 'DigDiscord.settings.deploy_ci')
     def test_loadsrvmsg_stores_in_db(self):
         """test if managed cmd loadsrvmsg is responding
         and loads data in db"""
@@ -17,7 +20,9 @@ class LoadSrvMsg(TransactionTestCase):
 
         call_command("loadsrvmsg", **kwargs)
         # TODO verify that we get a channel object with the associated msg
-
+        
+    @unittest.skipIf(
+            os.getenv(name,'DJANGO_SETTINGS_MODULE') == 'DigDiscord.settings.deploy_ci')
     def test_loadsrvmsg_all_channels(self):
         """ same test for all channels """
 
