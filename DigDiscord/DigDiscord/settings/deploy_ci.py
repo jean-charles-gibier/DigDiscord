30d2ba91fbcf0f21ba87b3d617897e969896fd21
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-12-20 21:04:01 +0100

    Update deploy_ci.py

--- DigDiscord/DigDiscord/settings/deploy_ci.py
+++ DigDiscord/DigDiscord/settings/deploy_ci.py
@@ -15,7 +15,7 @@ DATABASES = {
         # Tell Django to build the test database with the 'utf8mb4' character set
         "TEST": {
             "USER": "user",
-            "NAME": "digdiscord",
+            "NAME": "digdiscord_test",
             "PASSWORD": "MOTDEPASSE1234",
             "CHARSET": "utf8mb4",
             "COLLATION": "utf8mb4_unicode_ci",
