c5f3eb97eda563139d2292a74df5f7612addc5f8
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-12-20 16:33:40 +0100

    .gitignore exclude flake8

---
+++ DigDiscord/DigDiscord/settings/deploy_ci.py
@@ -0,0 +1,21 @@
+from . import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "digdiscord",
+        "USER": "user",
+        #        "PASSWORD": "MOTDEPASSE1234",
+        "HOST": "172.18.0.1",
+        "PORT": "3306",
+        "OPTIONS": {
+            # Tell MySQLdb to connect with 'utf8mb4' character set
+            "charset": "utf8mb4",
+        },
+        # Tell Django to build the test database with the 'utf8mb4' character set
+        "TEST": {
+            "CHARSET": "utf8mb4",
+            "COLLATION": "utf8mb4_unicode_ci",
+        },
+    }
+}
