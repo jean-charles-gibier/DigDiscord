66962a4cd4da974f6fe93455d8eefc967b5cdd89
Jean-Charles Gibier <jeancharlesg@free.fr>
2021-03-05 17:46:09 +0100

    Changement de nom de domaine #3

--- DigDiscord/api/urls.py
+++ DigDiscord/api/urls.py
@@ -54,18 +54,18 @@ urlpatterns = [
     path("profile/", ProfileManager.as_view(), name="profile_manager"),
 ]
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#   openapi.Info(
+#      title="Snippets API",
+#      default_version='v1',
+#      description="Test description",
+#      terms_of_service="https://www.google.com/policies/terms/",
+#      contact=openapi.Contact(email="contact@snippets.local"),
+#      license=openapi.License(name="BSD License"),
+#   ),
+#   public=True,
+#   permission_classes=(permissions.AllowAny,),
+#)
 
 # urlpatterns = urlpatterns + [
 #        path(r'swagger(?<str:format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
