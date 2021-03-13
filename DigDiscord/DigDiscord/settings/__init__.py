19eaf9c8cd565784d72a404de3cebc3ee47b5117
Jean-Charles Gibier <jeancharlesg@free.fr>
2021-03-13 12:49:03 +0100

    Installed APPS = drf_yasg => swagger

--- DigDiscord/DigDiscord/settings/__init__.py
+++ DigDiscord/DigDiscord/settings/__init__.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-#    'drf_yasg',
+    'drf_yasg',
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
