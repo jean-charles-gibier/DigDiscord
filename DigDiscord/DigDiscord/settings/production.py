ff37c400f4903d3c358163ac806a849878ee142d
Jean-Charles Gibier <jeancharlesg@free.fr>
2021-03-21 23:25:28 +0100

    Suppression du mode debug des settings

--- DigDiscord/DigDiscord/settings/production.py
+++ DigDiscord/DigDiscord/settings/production.py
@@ -12,19 +12,19 @@ DATABASES = {
         "HOST": os.environ.get('PRODUCTION_BD_HOST'),
         "PORT": os.environ.get('PRODUCTION_BD_PORT'),
         "OPTIONS": {
-	    "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
             "charset": "utf8mb4",
         },
         # Tell Django to build the test database with the 'utf8mb4' character set
         "TEST": {
             "CHARSET": "utf8mb4",
             "COLLATION": "utf8mb4_unicode_ci",
-             "USER": "rout",
+            "USER": "rout",
             "NAME": "digdiscord_test",
-#           "PASSWORD": "MOTDEPASSE1234",
+            #           "PASSWORD": "MOTDEPASSE1234",
         },
     }
 }
 
-
 ALLOWED_HOSTS.append("tgpa8220.odns.fr")
+DEBUG = False
