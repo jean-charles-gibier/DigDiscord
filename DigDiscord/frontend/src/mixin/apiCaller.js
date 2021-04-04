e7fef7122ef70e2e3b125ae36d6953826088f492
Jean-Charles Gibier <jeancharlesg@free.fr>
2021-04-04 16:00:30 +0200

    Correction bug saisie user / token #3

--- DigDiscord/frontend/src/mixin/apiCaller.js
+++ DigDiscord/frontend/src/mixin/apiCaller.js
@@ -56,7 +56,7 @@ export default {
           finalRet = errstr
         })
       } catch (ex) {
-        console.log('==>', ex.message)
+        console.log('Exception ==>', ex.message)
       } finally { }
       return finalRet
     },
