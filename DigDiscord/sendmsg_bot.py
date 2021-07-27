672814b4a351cd3f7097bfd51d97900bc3b7f701
Jean-Charles Gibier <jeancharlesg@free.fr>
2021-07-27 19:23:05 +0200

    Ajout du bot et du module discord #3

--- DigDiscord/sendmsg_bot.py
+++ DigDiscord/sendmsg_bot.py
@@ -2,24 +2,29 @@
 # https://github.com/Rapptz/discord.py/blob/v1.7.3/examples/background_task.py
 
 import discord
+import os
+import sys
 
-my_token = 'NzE5MTMxNDM4ODg4MjU1NTMx.Xty9Vw.hu0m-1l8Qw-GD4DkAZxsbhlWFBE'
+my_token = os.environ.get('BOT_TOKEN')
 
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def set_stat_link(self, link):
+        self.link = link
 
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
         print('------')
-        channel = self.get_channel(778208282573275139) # channel ID goes here
-        await channel.send("What a wonderful world !")
+        channel = self.get_channel(os.environ.get('BOT_CHANNEL')) # channel ID goes here
+        await channel.send(f"Top contributeurs de la semaine !\n {self.link}")
         await self.close()
 
 
 client = MyClient()
+client.set_stat_link(sys.argv[1])
 client.run(my_token)
 exit()
